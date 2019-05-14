b138a6fc1f9068bf2782a327be0b1c9fa28b8493
mrFermin216 <50597331+mrFermin216@users.noreply.github.com>
2019-05-14 07:31:07 -0700

    Add files via upload

---
+++ metodos1/metodos1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace metodos1
+{
+    class Program
+    {
+        static void Imprimir(int[] numeros)
+        {
+            foreach(int n in numeros)
+            {
+                Console.WriteLine("{0} ", n);
+            }
+        }
+        static int[] GenerarTabla(int numInicial)
+        {
+            
+            int[] numerosTabla = new int[10];
+            int res = numInicial;
+            for (int i = 0; i < numerosTabla.Length; i++)
+            {
+                numerosTabla[i] = res;
+                res += numInicial;
+            }
+            return numerosTabla;
+        }
+        static void Main(string[] args)
+        {
+            int numerador = 2, denominador = 1;
+            Racional fraccion = new Racional(denominador, numerador);
+            Console.WriteLine(fraccion.ToString());
+            fraccion.numerador = int.Parse(Console.ReadLine());
+            Console.WriteLine(fraccion.ToString());
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int[] numeros = GenerarTabla(int.Parse(Console.ReadLine()));
+            foreach(int n in numeros)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("{0} es par",n);
+                }
+                else
+                {
+                    Console.WriteLine("{0} es impar",n);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ metodos1/metodos1/Racional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodos1
+{
+    class Racional
+    {
+        public int numerador { get; set; }
+        public int denominador { get; set; }
+
+        public Racional (int numerador, int denominador)
+        {
+            this.numerador = numerador;
+            this.denominador = denominador;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", numerador, denominador);
+        }
+    }
+}
